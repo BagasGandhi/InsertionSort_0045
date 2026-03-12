e2c2522d15eb58c97eddd28d71ac5f359d29b07e
BagasGandhi <saputramuhammadgandhi@gmail.com>
2026-03-12 19:24:05 +0800

    Refactor input and insertion sort procedures for improved readability and consistency

--- insertion_sort.cpp
+++ insertion_sort.cpp
@@ -4,15 +4,19 @@ using namespace std;
 int arr[20]; //membuat array dengan panjang data 20
 int n;       //membuat variable inputan n
 
-void input(){ //procedure input
-    while (true){
+void input()
+{ //procedure input
+    while (true)
+    {
         cout << "Masukkan Jumlah Data Pada Array : ";
         cin >> n; 
 
-        if (n <= 20){
+        if (n <= 20)
+        {
             break;
         }
-        else{
+        else
+        {
             cout << "\nArray Tidak Boleh lebih dari 20\n";
         }
     }
@@ -21,18 +25,34 @@ void input(){ //procedure input
     cout << "Masukkan Element Array" << endl;
     cout << "===================" << endl;
 
-    for (int i = 0; i < n; i++ ){
+    for (int i = 0; i < n; i++ )
+    {
         cout << "Data ke-" << (i+1) << ": ";
         cin >> arr[i];
     }
 }
 
-void insertionsort(){ //procedure insertionsort
+void insertionsort()
+{ //procedure insertionsort
     int temp;
     int j;
 
-    for (int i = 0; i < n; i++){
+    for (int i = 0; i < n; i++)
+    {
         temp = arr[i];
         j = i-1;
+        while (j >= 0 && arr[j] > temp)
+        {
+            arr[j + 1] = arr[j];
+            j--;
+        }
+        arr[j + 1] = temp;
+        cout << "\nStep " << i << ": ";
+        for (int k = 0; k < n; k++)
+        {
+            cout << arr[k] << " ";
+        }
     }
 }
+
+
