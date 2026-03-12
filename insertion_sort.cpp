ac9c15e6ff905d1210417236369a09f592fc5022
BagasGandhi <saputramuhammadgandhi@gmail.com>
2026-03-12 19:11:21 +0800

    Implement input procedure for array size validation in insertion_sort.cpp

--- insertion_sort.cpp
+++ insertion_sort.cpp
@@ -4,3 +4,14 @@ using namespace std;
 int arr[20]; //membuat array dengan panjang data 20
 int n;       //membuat variable inputan n
 
+void input(){ //procedure input
+    while (true){
+        cout << "Masukkan Jumlah Data Pada Array : ";
+        cin >> n; 
+
+        if (n <= 20)
+            break;
+        else
+            cout << "\nArray Tidak Boleh lebih dari 20\n";
+    }
+}
