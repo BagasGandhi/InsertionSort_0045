143182758b934563d5588de8904252db22d529a9
BagasGandhi <saputramuhammadgandhi@gmail.com>
2026-03-12 19:26:45 +0800

    Add display procedure to output array elements

--- insertion_sort.cpp
+++ insertion_sort.cpp
@@ -55,4 +55,16 @@ void insertionsort()
     }
 }
 
-
+void display()
+{
+    cout << endl;
+    cout << "\n===================" << endl;
+    cout << "Element Array " << endl;
+    cout << "\n===================" << endl;
+    
+    for(int j = 0; j < n; j++)
+    {
+        cout << arr[j] << endl;
+    }
+    cout << endl;
+}
