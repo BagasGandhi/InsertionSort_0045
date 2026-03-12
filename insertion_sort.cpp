ae44b710032a07d5692d0bef7a0ee0ffe166ca0f
BagasGandhi <saputramuhammadgandhi@gmail.com>
2026-03-12 19:14:41 +0800

    Refactor input procedure to include array element input loop

--- insertion_sort.cpp
+++ insertion_sort.cpp
@@ -20,5 +20,8 @@ void input(){ //procedure input
     cout << "Masukkan Element Array" << endl;
     cout << "===================" << endl;
 
-    
+    for (int i = 0; i < n; i++ ){
+        cout << "Data ke-" << (i+1) << ": ";
+        cin >> arr[i];
+    }
 }
